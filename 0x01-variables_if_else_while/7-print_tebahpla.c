32aee9519ea8fbeab68850502125085645050a6d
Akram-Elghalbzouri <elghalbzouri.akram@gmail.com>
2023-02-17 17:20:21 +0100

    Completed task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 122; i <= 97; i--)
+putchar(i);
+putchar('\n');
+return (0);
+}
