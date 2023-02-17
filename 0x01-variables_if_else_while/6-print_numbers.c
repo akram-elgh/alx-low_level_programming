199d89af507e929005d1f7e2b1f42fd6f057094c
Akram-Elghalbzouri <elghalbzouri.akram@gmail.com>
2023-02-17 17:16:47 +0100

    Completed task 6

---
+++ 0x01-variables_if_else_while/6-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
