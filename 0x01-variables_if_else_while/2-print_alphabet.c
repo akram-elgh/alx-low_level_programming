4d7213afdeef8cbbe444105eed4c0558d426cba9
Akram-Elghalbzouri <elghalbzouri.akram@gmail.com>
2023-02-17 16:12:21 +0100

    Completed task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+for (int i = 97; i <= 122; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
