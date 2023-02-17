d94047b2e4ec5b97879390e86fbf2dfda0624797
Akram-Elghalbzouri <elghalbzouri.akram@gmail.com>
2023-02-17 18:53:37 +0100

    Completed advanced task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+int j;
+for (i = 48; i < 58; i++)
+{
+for (j = i + 1; j < 58; j++)
+{
+putchar(i);
+putchar(j);
+if (i < 56)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
