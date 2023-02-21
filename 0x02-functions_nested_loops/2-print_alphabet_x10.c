e4c0a87ffef88c117a40cf6917c8deee36a12c10
Akram-Elghalbzouri <elghalbzouri.akram@gmail.com>
2023-02-21 14:36:46 +0100

    Updating documentation

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,6 +2,8 @@
 
 /**
  * print_alphabet_x10 - pa function that prints 10 times the alphabet, in lowercase, followed by a new line.
+ * 
+ * Return: void
  */
 void print_alphabet_x10(void)
 {
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,10 @@
 #include "main.h"
 
 /**
- * _islower - a function that checks for lowercase character.
- * Return: 1 if c is lowercase, 0 otherwise
+ * _islower - Check if a character is lowercase
+ * @c: Character to be checked
+ * 
+ * Return: 1 if c is lowercase. 0 otherwise
  */ 
 int _islower(int c)
 {
