6fb7dc58b86d22e94ddf37170b64e2ff8f734c1d
Akram-Elghalbzouri <elghalbzouri.akram@gmail.com>
2023-02-21 12:26:23 +0100

    Completed task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+printf("_putchar");
+return (0);
+}
