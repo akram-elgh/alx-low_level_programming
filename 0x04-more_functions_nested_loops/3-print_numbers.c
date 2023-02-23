820e223b8ea48143518b93c41b37025ddffb70cc
Akram-Elghalbzouri <elghalbzouri.akram@gmail.com>
2023-02-23 22:05:51 +0100

    Completed task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_number - prints numbers from 0 to 9
+* 
+* Return: void
+*/
+void print_number(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
