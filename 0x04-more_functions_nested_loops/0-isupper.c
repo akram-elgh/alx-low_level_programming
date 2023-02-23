1b9e9f0c52ca940797e6e62e65be00fc9d53690b
Akram-Elghalbzouri <elghalbzouri.akram@gmail.com>
2023-02-23 20:28:32 +0100

    Compleetd task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* isupper - checks if a character is uppercase
+* @c: character to check
+* 
+* Return: 1 if c is upper, 0 otherwise
+*/
+int isupper(char c)
+{
+return (c >= 'a' && c <= 'z');
+}
