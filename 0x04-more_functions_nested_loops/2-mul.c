1100035aa708016cb84d8e1b2212a59043e14c2d
Akram-Elghalbzouri <elghalbzouri.akram@gmail.com>
2023-02-23 22:01:55 +0100

    Completed task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul - multiply two integers
+* @a: first integer
+* @b: second integer
+* 
+* Return: 1 if c is a digit, 0 otherwise
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
