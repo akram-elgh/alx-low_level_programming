f7e427f061e657be4e1902ea9069a5bc1c8d15a2
Akram-Elghalbzouri <elghalbzouri.akram@gmail.com>
2023-02-27 23:21:27 +0100

    Completing all tasks

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * reset_to_98 -a function that swaps the values of two integers.
+ * @n: The pointer to an int.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - a function that prints n elements 
+ * of an array of integers, followed by a new line.
+ * @a: The array of integers.
+ * @n: The number of elements to be printed.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == 0)
+			printf("%d", a[i]);
+		else
+			printf(", %d", a[i]);
+	}
+		printf("\n");
+}
