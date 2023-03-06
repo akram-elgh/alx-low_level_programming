910df9c64f20377b55dcfb6ac649804f3387435d
Akram-Elghalbzouri <elghalbzouri.akram@gmail.com>
2023-03-07 00:02:25 +0100

    Completing all tasks

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * infinite_add - add 2 integers.
+ * @n1: integer
+ * @n2: integer
+ * @r: buffer
+ * @size_r: size of r
+ * 
+ * Return: char
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int a;
+	int b;
+	a = atoi(n1);
+	b = atoi(n2);
+}
